567a808dd58c041eb0fdc5da599700bffe7645a6
Hyewon Lee <vndnsla12@gmail.com>
2024-08-16 15:01:17 +0900

    .

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].js',
-        path: path.join(__dirname, ''),
-        publicPath: '',
+        path: path.join(__dirname, 'docs'),
+        publicPath: '/HyewonLeeVue',
     },
 }
