231279258bc7d2f72e9c471d279e34e44a731152
Hyewon Lee <vndnsla12@gmail.com>
2024-08-16 11:13:55 +0900

    vue.config.js 추가

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].js',
-        path: path.join(__dirname, 'LeeHyewon'),
-        publicPath: '/LeeHyewon',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist',
     },
 }
